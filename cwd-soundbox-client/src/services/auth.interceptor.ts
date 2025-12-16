6dde3ac989879e352178a84c12a1a58bd3e10ea5
kiranraoboinapally <kiranraoboinapally@gmail.com>
2025-12-16 12:33:29 +0530

    token,db

--- cwd-soundbox-client/src/services/auth.interceptor.ts
+++ cwd-soundbox-client/src/services/auth.interceptor.ts
@@ -6,7 +6,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   // Retrieve the authentication token from localStorage.
   // ⚠️ Replace 'your_token_key' with the actual key you use after login.
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiZXhwIjoxNzY1NTIzMTM5LCJpYXQiOjE3NjU0MzY3Mzl9.o150LQS0DGcCJyyZRjsD2PqHKtvpn0pvadUKyoOVRz0";
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiZXhwIjoxNzY1OTU0ODE1LCJpYXQiOjE3NjU4Njg0MTV9.Oulo_q07TweOKh2eRoRKSxNLOSt4apk0KSgXG--TIWk";
 
   // If a token exists, clone the request to add the new header.
   if (token) {
